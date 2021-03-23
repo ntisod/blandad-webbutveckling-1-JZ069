0c149a23f4a9325c174a95e7fe3299eb5db410d7
JZ069 <72020096+JZ069@users.noreply.github.com>
2021-03-23 16:12:34 +0100

    Uppgift4

--- javascript-exempel.js
+++ javascript-exempel.js
@@ -3,6 +3,26 @@ som kan sträcka över flera rader. */
 
 //En kommentar på en rad.
 
-function visaDialog(){
-    alert('Hej på dig!');
+function Uppgift2(){
+    alert('Skapad av Jakub 2021/03/23');
 }
+
+function Uppgift3(){
+    alert('Jakub');
+    changeText();
+    document.getElementById("demo").style.backgroundColor = "lightblue";
+}
+
+function changeText(){
+    document.getElementById("demo").innerHTML = "Hello javaScript"
+}
+
+function Uppgift4(sw) {
+    var pic;
+    if (sw == 0) {
+      pic = "bild1.jpg"
+    } else {
+      pic = "Bild2.jpg"
+    }
+    document.getElementById('myImage').src = pic;
+  }
